a00d42be3edd3f8972751d629cae13c61d683212
cleversonrossi <47364527+cleversonrossi@users.noreply.github.com>
2019-04-27 12:24:07 -0300

    Construindo filtros
    Esse merge não contém somente códigos dos filtros.

--- src/main/java/com/financeiro/api/resource/LancamentoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.financeiro.api.resource;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.financeiro.api.model.Lancamento;
-
-public interface LancamentoRepository extends JpaRepository<Lancamento, Long> {
-
-}
--- src/main/java/com/financeiro/api/resource/LancamentoResource.java
+++ src/main/java/com/financeiro/api/resource/LancamentoResource.java
@@ -1,5 +1,6 @@
 package com.financeiro.api.resource;
 
+import java.util.Arrays;
 import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
@@ -7,8 +8,11 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,7 +21,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.financeiro.api.event.RecursoCriadoEvent;
+import com.financeiro.api.exceptionhandler.FinanceiroExceptionHandler.Erro;
 import com.financeiro.api.model.Lancamento;
+import com.financeiro.api.repository.LancamentoRepository;
+import com.financeiro.api.repository.filter.LancamentoFilter;
+import com.financeiro.api.service.LancamentoService;
+import com.financeiro.api.service.exception.PessoaInexistenteOuInativaException;
 
 @RestController
 @RequestMapping("/lancamentos")
@@ -27,11 +36,17 @@ public class LancamentoResource {
 	private LancamentoRepository lancamentoRepository;
 	
 	@Autowired
+	private LancamentoService lancamentoService;
+	
+	@Autowired
 	private ApplicationEventPublisher publisher;
 	
+	@Autowired
+	private MessageSource messageSource;
+	
 	@GetMapping
-	public List<Lancamento> listar() {
-		return lancamentoRepository.findAll();
+	public List<Lancamento> pesquisar(LancamentoFilter lancamentoFilter) {
+		return lancamentoRepository.filtrar(lancamentoFilter);
 	}
 	
 	@GetMapping("/{codigo}")
@@ -42,9 +57,17 @@ public class LancamentoResource {
 	
 	@PostMapping
 	public ResponseEntity<Lancamento> criar(@Valid @RequestBody Lancamento lancamento, HttpServletResponse response) {
-		Lancamento lancamentoSalvo = lancamentoRepository.save(lancamento);
+		Lancamento lancamentoSalvo = lancamentoService.salvar(lancamento);
 		publisher.publishEvent(new RecursoCriadoEvent(this, response, lancamentoSalvo.getCodigo()));
 		return ResponseEntity.status(HttpStatus.CREATED).body(lancamentoSalvo);
+	}
+	
+	@ExceptionHandler({ PessoaInexistenteOuInativaException.class })
+	public ResponseEntity<Object> handlePessoaInexistenteOuInativaException(PessoaInexistenteOuInativaException ex) {
+		String mensagemUsuario = messageSource.getMessage("pessoa.inexistente-ou-inativa", null, LocaleContextHolder.getLocale());
+		String mensagemDesenvolvedor = ex.toString();
+		List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor));
+		return ResponseEntity.badRequest().body(erros);
 	}
 	
 }
